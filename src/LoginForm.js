42533c47ce977fe75c1fc600b539ba7720365c08
Colby <jbcolby0063@gmail.com>
2021-04-11 16:57:37 -0500

    first frontend commit

---
+++ src/LoginForm.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import React from 'react';
+
+class LoginForm extends React.Component {
+  render() {
+    return (
+      <div className="loginForm">
+        asdad
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
